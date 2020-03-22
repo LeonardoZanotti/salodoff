4f451b77ef442a84b8d46d01d5dab76178d5d7ba
Leonardo <leonardojosezanotti@gmail.com>
2020-03-22 04:51:20 -0300

    Atualização ass

--- commands/ass.js
+++ commands/ass.js
@@ -5,8 +5,8 @@ exports.run = async (bot, message, args) => {
     //for(i = 0; i < (assP.length + 1); i++) {
         const embed = new Discord.RichEmbed()
             .setColor('RANDOM')
-            .setImage(assP[args[0]])
-            //.setImage(assP[Math.floor(Math.random() * assP.length)]);
+            //.setImage(assP[args[0]])
+            .setImage(assP[Math.floor(Math.random() * assP.length)]);
         return message.channel.send(embed);
     //}
 }
