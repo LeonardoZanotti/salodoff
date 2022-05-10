2fa123cf612271af447e968259f650bc30e23e25
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-05-10 17:00:34 -0300

    packages update and fixes

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,17 +3,17 @@ const moment = require('moment');
 moment.locale('pt-BR');
 
 exports.run = (bot, message, args) => {
-    let gAvatar = message.guild.iconURL;
-    const user = message.author;
-    
-    let membros = message.guild.memberCount;
-    let bots = message.guild.members.filter(member => member.user.bot).size
-    let total = membros - bots
+  let gAvatar = message.guild.iconURL;
+  const user = message.author;
 
-    let embed = new Discord.RichEmbed()
+  let membros = message.guild.memberCount;
+  let bots = message.guild.members.filter((member) => member.user.bot).size;
+  let total = membros - bots;
+
+  let embed = new Discord.RichEmbed()
     .setTimestamp()
     .setTitle(`${message.guild.name}`)
-    .setColor("RANDOM")
+    .setColor('RANDOM')
     .setThumbnail(gAvatar)
     .setDescription('Informações sobre o servidor')
     .addField(`ID do servidor:`, message.guild.id)
@@ -25,25 +25,52 @@ exports.run = (bot, message, args) => {
     .addField('Piloto de caça do servidor:', 'Godinho')
     .addField('Corno do servidor:', 'Godoy')
     .addField('Consultor de vendas do servidor:', 'Yoshi')
-    .addField('Região do servidor:', message.guild.region)   
+    .addField('Região do servidor:', message.guild.region)
     .addField('Servidor criado em:', moment(message.guild.createdAt).format('DD/MM/YYYY'))
     .addField(`${user.username} entrou no servidor em:`, moment(message.member.joinedAt).format('DD/MM/YYYY'))
     .addField('Eu entrei aqui em:', moment(bot.user.joinedAt).format('DD/MM/YYYY'))
-    .addField(`Total de canais:`, `${message.guild.channels.filter(channels => channels.type === 'voice').size} voz / ${message.guild.channels.filter(channels => channels.type === 'text').size} texto`)
-    .addField(`Cargos [${message.guild.roles.size - 1}]:`, message.guild.roles.map(r => r).join(' ').replace('@everyone', ''))
+    .addField(
+      `Total de canais:`,
+      `${message.guild.channels.filter((channels) => channels.type === 'voice').size} voz / ${
+        message.guild.channels.filter((channels) => channels.type === 'text').size
+      } texto`
+    )
+    .addField(
+      `Cargos [${message.guild.roles.size - 1}]:`,
+      message.guild.roles
+        .map((r) => r)
+        .join(' ')
+        .replace('@everyone', '')
+    )
     .addField('Humanos:', total, true)
     .addField('Bots:', bots, true)
     .addField('Total de membros:', membros, true)
-    .addField('Admins Online', `:green_circle: ${message.guild.members.filter(o => o.presence.status === 'online').size} Online`, true)
-    .addField('Admins Offline', `:red_circle: ${message.guild.members.filter(o => o.presence.status === 'offline').size} Offline`, true)
-    .addField('Admins Idle', `:orange_circle: ${message.guild.members.filter(o => o.presence.status === 'idle').size} Idle`, true)
-    .addField('Admins Do not Disturb', `:no_entry: ${message.guild.members.filter(o => o.presence.status === 'dnd').size} Do not Disturb`, true)
-    .setFooter('Serverinfo', bot.user.displayAvatarURL)
+    .addField(
+      'Admins Online',
+      `:green_circle: ${message.guild.members.filter((o) => o.presence.status === 'online').size} Online`,
+      true
+    )
+    .addField(
+      'Admins Offline',
+      `:red_circle: ${message.guild.members.filter((o) => o.presence.status === 'offline').size} Offline`,
+      true
+    )
+    .addField(
+      'Admins Idle',
+      `:orange_circle: ${message.guild.members.filter((o) => o.presence.status === 'idle').size} Idle`,
+      true
+    )
+    .addField(
+      'Admins Do not Disturb',
+      `:no_entry: ${message.guild.members.filter((o) => o.presence.status === 'dnd').size} Do not Disturb`,
+      true
+    )
+    .setFooter('Serverinfo', bot.user.displayAvatarURL);
 
-    message.channel.send(embed);
-}
+  message.channel.send(embed);
+};
 
 exports.help = {
-    name: 'serverinfo',
-    description: `Informações sobre o servidor`
-}
+  name: 'serverinfo',
+  description: `Informações sobre o servidor`,
+};
